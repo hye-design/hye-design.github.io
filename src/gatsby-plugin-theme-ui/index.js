5acdb69913a1cc9f5ddfe66cc339a7034787751c
seonhye <seonae__p@naver.com>
2022-03-09 02:43:59 +0900

    2022.03.09 theme

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -5,6 +5,9 @@ const theme = merge(originalTheme, {
   fonts: {
     body: `pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, 'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', sans-serif;`,
   },
+  fontSizes: [
+    14, 16, 16, 18, 24, 32, 36, 32
+  ],
 });
 
 export default theme;
